b97553e6b35585614af36f352ac946b3b341bd34
Tuanbn <tuanbn.hedspi@gmail.com>
2014-01-09 11:41:28 +0700

    update image and funs

--- src/com/example/camera102/MainActivity.java
+++ src/com/example/camera102/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.camera102;
 
 import java.io.File;
-import com.example.camera102.ImageEditor;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -19,11 +17,10 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
 
@@ -36,28 +33,19 @@ public class MainActivity extends Activity implements OnClickListener {
 	final static int ABOUT = 0;
 
 	final static String CAMERA_TMP_FILE = "cmr102.jpg";
-	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 200;
-
-	private Button choosePictureButton, chooseVideoButton, takePictureButton, exit;
+	private static final int CAPTURE_IMAGE = 200;
+	public static final int MEDIA_TYPE_IMAGE = 100;
+	private static final String IMAGE_DIRECTORY_NAME = "Camera102";
+	private Button choosePictureButton, chooseVideoButton, takePictureButton,
+			exit;
 	private ImageView image;
-	private Uri uriCameraImage = null;
+	private Uri uriCameraImage;
+	private String selectedImagePath;
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-		deleteTmpFile();
 
-	}
-
-	private void deleteTmpFile() {
-		File fileDir = getExternalFilesDir(null);
-
-		if (fileDir == null || !fileDir.exists())
-			fileDir = getFilesDir();
-
-		File tmpFile = new File(fileDir, CAMERA_TMP_FILE);
-		if (tmpFile.exists())
-			tmpFile.delete();
 	}
 
 	@Override
@@ -65,7 +53,6 @@ public class MainActivity extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setLayout();
-		deleteTmpFile();
 
 	}
 
@@ -86,172 +73,144 @@ public class MainActivity extends Activity implements OnClickListener {
 		exit.setOnClickListener(this);
 
 	}
-	/*Onclick
-	 Event button exit, choose picture and video onclick
-	@author 11A Bach Ngoc Tuan*/
+
+	/*
+	 * Onclick Event button exit, choose picture and video onclick
+	 * 
+	 * @author 11A Bach Ngoc Tuan
+	 */
 	public void onClick(View v) {
 		if (v == exit) {
 			finish();
 			System.exit(0);
 		} else if (v == choosePictureButton) {
-
-			try {
-				 setContentView(R.layout.mainloading);
-				Intent intent = new Intent(Intent.ACTION_PICK);
-				intent.setType("image/*"); // limit to image types for now
-				startActivityForResult(intent, GALLERY_RESULT);
-
-			} catch (Exception e) {
-				Toast.makeText(this, "Unable to open Gallery app",
-						Toast.LENGTH_LONG).show();
-				Log.e(TAG,
-						"error loading gallery app to choose photo: "
-								+ e.getMessage(), e);
-			}
+			setContentView(R.layout.mainloading);
+			Intent intent = new Intent();
+			intent.setType("image/*");
+			intent.setAction(Intent.ACTION_GET_CONTENT);
+			startActivityForResult(
+					Intent.createChooser(intent, "Select Picture"),
+					GALLERY_RESULT);
 
 		} else if (v == chooseVideoButton) {
 
-			try {
-				setContentView(R.layout.mainloading);
-				Intent intent = new Intent(Intent.ACTION_PICK);
-				intent.setType("video/*"); // limit to video types for now
-				startActivityForResult(intent, GALLERY_RESULT);
-
-			} catch (Exception e) {
-				Toast.makeText(this, "Unable to open Gallery app",
-						Toast.LENGTH_LONG).show();
-				Log.e(TAG,
-						"error loading gallery app to choose photo: "
-								+ e.getMessage(), e);
-			}
+			setContentView(R.layout.mainloading);
+			Intent intent = new Intent();
+			intent.setType("video/*");
+			intent.setAction(Intent.ACTION_GET_CONTENT);
+			startActivityForResult(
+					Intent.createChooser(intent, "Select Video"),
+					GALLERY_RESULT);
 
 		} else if (v == takePictureButton) {
-			/*Onclick
-			 Event button takePicture
-			@author 11C Dang Xuan Binh*/
-           setContentView(R.layout.mainloading);
-			
-			String storageState = Environment.getExternalStorageState();
-	        if(storageState.equals(Environment.MEDIA_MOUNTED)) {
-
-	          
-	            ContentValues values = new ContentValues();
-	          
-	            values.put(MediaStore.Images.Media.TITLE, CAMERA_TMP_FILE);
-	      
-	            values.put(MediaStore.Images.Media.DESCRIPTION,"ssctmp");
-
-	            File tmpFileDirectory = new File(Environment.getExternalStorageDirectory().getPath() + ImageEditor.TMP_FILE_DIRECTORY);
-	            if (!tmpFileDirectory.exists())
-	            	tmpFileDirectory.mkdirs();
-	            
-	            File tmpFile = new File(tmpFileDirectory,"cam" + ImageEditor.TMP_FILE_NAME);
-	        	
-	        	uriCameraImage = Uri.fromFile(tmpFile);
-	            //uriCameraImage = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-
-	            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE );
-	            intent.putExtra( MediaStore.EXTRA_OUTPUT, uriCameraImage);
-	            
-	            startActivityForResult(intent, CAMERA_RESULT);
-	        }   else {
-	            new AlertDialog.Builder(MainActivity.this)
-	            .setMessage("External Storeage (SD Card) is required.\n\nCurrent state: " + storageState)
-	            .setCancelable(true).create().show();
-	        }
-	        
-			takePictureButton.setVisibility(View.VISIBLE);
-			choosePictureButton.setVisibility(View.VISIBLE);
-			chooseVideoButton.setVisibility(View.VISIBLE);
-			
-		} 
-	}
-/*onActivityResult
- * @author 11A Bach Ngoc Tuan
- * */
-	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		
-		
-		if (resultCode == RESULT_OK)
-		{
+			/*
+			 * Onclick Event button takePicture
+			 * 
+			 * @author 11C Dang Xuan Binh
+			 */
 			setContentView(R.layout.mainloading);
-			
-			if (requestCode == GALLERY_RESULT) 
-			{
-				if (intent != null)
-				{
-					Uri uriGalleryFile = intent.getData();
-					
-					try
-						{
-							if (uriGalleryFile != null)
-							{
-								Cursor cursor = managedQuery(uriGalleryFile, null, 
-		                                null, null, null); 
-								cursor.moveToNext(); 
-								// Retrieve the path and the mime type 
-								String path = cursor.getString(cursor 
-								                .getColumnIndex(MediaStore.MediaColumns.DATA)); 
-								String mimeType = cursor.getString(cursor 
-								                .getColumnIndex(MediaStore.MediaColumns.MIME_TYPE));
-								
-								if (mimeType == null || mimeType.startsWith("image"))
-								{
-									
-								}
-								else if (mimeType.startsWith("video"))
-								{
-		
-									
-								}
-							}
-							else
-							{
-								Toast.makeText(this, "Unable to load media.", Toast.LENGTH_LONG).show();
-			
-							}
-						}
-					catch (Exception e)
-					{
-						Toast.makeText(this, "Unable to load media.", Toast.LENGTH_LONG).show();
-						Log.e(TAG, "error loading media: " + e.getMessage(), e);
+			Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			uriCameraImage = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+			intent.putExtra(MediaStore.EXTRA_OUTPUT, uriCameraImage);
+			startActivityForResult(intent, CAPTURE_IMAGE);
 
-					}
-				}
-				else
-				{
-					Toast.makeText(this, "Unable to load photo.", Toast.LENGTH_LONG).show();
-	
-				}
-					
-			}
-			else if (requestCode == CAMERA_RESULT)
-			{
-				//Uri uriCameraImage = intent.getData();
-				
-				if (uriCameraImage != null)
-				{
-					Intent passingIntent = new Intent(this,ImageEditor.class);
-					passingIntent.setData(uriCameraImage);
-					startActivityForResult(passingIntent,IMAGE_EDITOR);
-				}
-				else
-				{
-					takePictureButton.setVisibility(View.VISIBLE);
-					choosePictureButton.setVisibility(View.VISIBLE);
-				}
+		}
+	}
+	/*
+	 * Creating file uri to store image/video
+	 * @author 11C Dang Xuan Binh
+	 */
+	public Uri getOutputMediaFileUri(int type) {
+		return Uri.fromFile(getOutputMediaFile(type));
+	}
+	/*
+	 * getOutputMediaFile
+	 * @author 11C Dang Xuan Binh
+	 */
+	private static File getOutputMediaFile(int type) {
+
+		// External sdcard location
+		File mediaStorageDir = new File(
+				Environment
+						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+				IMAGE_DIRECTORY_NAME);
+
+		// Create the storage directory if it does not exist
+		if (!mediaStorageDir.exists()) {
+			if (!mediaStorageDir.mkdirs()) {
+				Log.d(IMAGE_DIRECTORY_NAME, "Oops! Failed create "
+						+ IMAGE_DIRECTORY_NAME + " directory");
+				return null;
 			}
 		}
-		else
-			setLayout();
-		
-		
-		
-	}	
+
+		// Create a media file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+				Locale.getDefault()).format(new Date());
+		File mediaFile;
+		if (type == MEDIA_TYPE_IMAGE) {
+			mediaFile = new File(mediaStorageDir.getPath() + File.separator
+					+ "IMG_" + timeStamp + ".jpg");
+		} else {
+			return null;
+		}
+
+		return mediaFile;
+	}
 	/*
-	 * onCreateOptionsMenu
-	 * CreateOptionsMenu 
-	 * @author 11A Bach Ngoc Tuan*/
+	 * previewCapturedImage()
+	 * @author 11C Dang Xuan Binh
+	 */
+	private void previewCapturedImage() {
+		try {
+
+			image.setVisibility(View.VISIBLE);
+
+			// bimatp factory
+			BitmapFactory.Options options = new BitmapFactory.Options();
+
+			// downsizing image as it throws OutOfMemory Exception for larger
+			// images
+			
+			final Bitmap bitmap = BitmapFactory.decodeFile(
+					uriCameraImage.getPath(), options);
+			image.setImageBitmap(bitmap);
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * onActivityResult
+	 * 
+	 * @author 11A Bach Ngoc Tuan
+	 */
+	protected void onActivityResult(int requestCode, int resultCode,
+			Intent intent) {
+
+		if (resultCode == RESULT_OK) {
+			setContentView(R.layout.mainloading);
+
+			if (requestCode == GALLERY_RESULT) {
+				setContentView(R.layout.imageviewer);
+				image = (ImageView) findViewById(R.id.ImageEditorImageView);
+				Uri selectedImageUri = intent.getData();
+				image.setImageURI(selectedImageUri);
+
+			} else if (requestCode == CAPTURE_IMAGE) {
+				setContentView(R.layout.imageviewer);
+				image = (ImageView) findViewById(R.id.ImageEditorImageView);
+				previewCapturedImage();
+			}
+		} else
+			setLayout();
+	}
+
+	/*
+	 * onCreateOptionsMenu CreateOptionsMenu
+	 * 
+	 * @author 11A Bach Ngoc Tuan
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
